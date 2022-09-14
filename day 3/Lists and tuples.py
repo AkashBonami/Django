4207b668b8b38071162d2959bac5dad5aaa7f139
Akash <akash.mittal@bonamisoftware.com>
2022-09-14 11:46:03 +0530

    lists tuples and dictionaries

---
+++ day 3/Lists and tuples.py
@@ -0,0 +1,6 @@
+List = ["Geek", "For", "Geeks"]
+print("\nList containing multiple values: ")
+# print(List[3])
+List2 = [['Geeks', 'For'], ['Geeks']]
+print("\nMulti-Dimensional List: ")
+print(List2[0][1])
---
+++ day 3/dictionaries.py
@@ -0,0 +1,28 @@
+# Dict = {'Name': 'Geeks', 1: [1, 2, 3, 4]}
+# print("Creating Dictionary: ")
+# print(Dict)
+ 
+# # accessing a element using key
+# print("Accessing a element using key:")
+# print(Dict['Name'])
+ 
+# # accessing a element using get()
+# # method
+# print("Accessing a element using get:")
+# print(Dict.get("Name"))
+ 
+# # creation using Dictionary comprehension
+# myDict = {x: x**2 for x in [1,2,3,4,5]}
+# print(myDict)
+list =[1,2,3,4,5,6,7]
+list1 =[4,5,6,7,8,9]
+list.extend(list1)
+print(list)
+print(list.__contains__(1,1))
+print(list)
+# tuple=tuple(list)
+# print(list)
+# print(tuple)
+# list[0]= 3
+# # newlist=list + tuple
+# print(list)
