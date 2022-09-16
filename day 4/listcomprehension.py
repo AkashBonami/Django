ca56ed5d057ca1bcc2c7aad7fc006d4430cdf8de
Akash <akash.mittal@bonamisoftware.com>
2022-09-16 11:36:52 +0530

    print name

---
+++ day 4/listcomprehension.py
@@ -0,0 +1,6 @@
+list = ['abc', 'def', 'abahshc','iuwriuewhr']
+newlist = [x  for x in list if "a" in x]
+print(newlist)
+# list comprehension is hot to write a 
+# for loop and if statement in one line
+print("AKASH")
